eedc05b6a3d5bd01050d478b411a32e36d5df16a
LW-Ho <leoleo41306@gmail.com>
2018-07-20 20:24:16 +0800

    update the code.

--- upload_data_requests.py
+++ upload_data_requests.py
@@ -3,10 +3,7 @@
 
 import requests
 
-class upload_data_requests(Base):
-
-  url = 'http://140.124.184.204:8082/~/in-cse/cnt-686670342'
-  
-  def send(mote,packet_tcflow,temperature,humidity,gasValue,gasAlarm):
-    raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { Mote:'+mote+', Priority:'+packet_tcflow+', EnvTemp:'+temperature+', EnvHumi:'+humidity+', EnvCO:'+gasValue+', Alarm:'+gasAlarm+' } "	} }'
-    requests.post(url, data=raw_data, headers={'X-M2M-Origin':'admin:admin','Content-Type':'application/json;ty=4'})
+url = 'http://140.124.184.204:8082/~/in-cse/cnt-686670342'
+def send(mote,packet_tcflow,temperature,humidity,gasValue,gasAlarm):
+  raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { Mote:'+mote+', Priority:'+packet_tcflow+', EnvTemp:'+temperature+', EnvHumi:'+humidity+', EnvCO:'+gasValue+', Alarm:'+gasAlarm+' } "	} }'
+  requests.post(url, data=raw_data, headers={'X-M2M-Origin':'admin:admin','Content-Type':'application/json;ty=4'})
