236416ee26255a103a0976eec783e20a741bac08
LW-Ho <leoleo41306@gmail.com>
2018-07-20 18:11:40 +0800

    add commit.

--- upload_data_requests.py
+++ upload_data_requests.py
@@ -1,4 +1,7 @@
-import requests,json
+# Using requests.
+# The url only for WSN.
+
+import requests
 
 url = 'http://140.124.184.204:8082/~/in-cse/cnt-686670342'
 raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { Mote:A6FB, Priority:1, EnvTemp:3888, EnvHumi:5435, EnvCO:25, Alarm:0 } "	} }'
