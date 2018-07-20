b7617a111d35617d4159c054442b7301849e9397
LW-Ho <leoleo41306@gmail.com>
2018-07-20 18:08:11 +0800

    the method can work.

--- upload_data_requests.py
+++ upload_data_requests.py
@@ -1,5 +1,5 @@
 import requests,json
 
 url = 'http://140.124.184.204:8082/~/in-cse/cnt-686670342'
-raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { Mote:A6F6, EnvTemp:3531, EnvHumi:6085, EnvCO:40, Alarm:0 } "	} }'
+raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { Mote:A6FB, Priority:1, EnvTemp:3888, EnvHumi:5435, EnvCO:25, Alarm:0 } "	} }'
 requests.post(url, data=raw_data, headers={'X-M2M-Origin':'admin:admin','Content-Type':'application/json;ty=4'})
