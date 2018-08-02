e766e96e0d3e3097b588a7692294fb0bddd1e5e1
LW-Ho <leoleo41306@gmail.com>
2018-08-02 13:10:32 +0800

    update upload data formate.

--- upload_data_requests.py
+++ upload_data_requests.py
@@ -6,5 +6,5 @@ import string
 
 url = 'http://140.124.184.204:8082/~/in-cse/cnt-686670342'
 def send(mote,packet_tcflow,temperature,humidity,gasValue,gasAlarm):
-  raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { Mote:'+mote+', Priority:'+str(packet_tcflow)+', EnvTemp:'+str(temperature)+', EnvHumi:'+str(humidity)+', EnvCO:'+str(gasValue)+', Alarm:'+str(gasAlarm)+' } "	} }'
+  raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { "Mote":"'+mote+'", "Priority":"'+str(packet_tcflow)+'", "EnvTemp":"'+str(temperature*0.01)+'", "EnvHumi":"'+str(humidity*0.01)+'", "EnvCO":"'+str(gasValue)+'", "Alarm":"'+str(gasAlarm)+'" } "	} }'
   requests.post(url, data=raw_data, headers={'X-M2M-Origin':'admin:admin','Content-Type':'application/json;ty=4'})
