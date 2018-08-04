4197dfafdd2dbf16f9f5e85625c2f0d67c2b25e6
LW-Ho <leoleo41306@gmail.com>
2018-08-04 16:51:34 +0800

    Successful, it can works.

--- upload_data_requests.py
+++ upload_data_requests.py
@@ -4,7 +4,11 @@
 import requests
 import string
 
-url = 'http://140.124.184.204:8082/~/in-cse/cnt-686670342'
+url = "http://140.124.184.204:8082/~/in-cse/cnt-686670342"
 def send(mote,packet_tcflow,temperature,humidity,gasValue,gasAlarm):
-  raw_data = '{"m2m:cin": { "cnf": "json",	"con": " { "Mote":"'+mote+'", "Priority":"'+str(packet_tcflow)+'", "EnvTemp":"'+str(temperature*0.01)+'", "EnvHumi":"'+str(humidity*0.01)+'", "EnvCO":"'+str(gasValue)+'", "Alarm":"'+str(gasAlarm)+'" } "	} }'
-  requests.post(url, data=raw_data, headers={'X-M2M-Origin':'admin:admin','Content-Type':'application/json;ty=4'})
+  payload = '{"m2m:cin": { "cnf": "json", "con": " { \\\"Mote\\\":\\\"'+mote+'\\\", \\\"Priority\\\":\\\"'+str(packet_tcflow)+'\\\",\\\"EnvTemp\\\":\\\"'+str(temperature*0.01)+'\\\",\\\"EnvHumi\\\":\\\"'+str(humidity*0.01)+'\\\",\\\"EnvCO\\\":\\\"'+str(gasValue)+'\\\",\\\"Alarm\\\":\\\"'+str(gasAlarm)+'\\\"  } " } }'
+  headers = {
+      'X-M2M-Origin': "admin:admin",
+      'Content-Type': "application/json;ty=4",
+      }
+  requests.post(url, data=payload, headers=headers)
